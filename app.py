fb7c47981a5ab8e2fdde2a7957e909178d61b9dc
RafalTaraska <paulo.dorosz@gmail.com>
2022-12-02 12:54:14 +0100

    v1.2

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
 from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
+@app.route('/mypage/me')
+def me():
     return render_template('index1.html')
 
-@app.route('/home2')
-def home2():
-    return render_template('index2.html')
-
-
-# @app.route('/innastrona', methods=['GET'])
-# def innastrona():
-#     return 'Witam na innej stronie'
-# @app.route('/klient/<numer>')
-# def klient(numer):
-#     return f'Klient o numerze {numer} to ...'
-# @app.route('/dodaj/<numer1>+<numer2>')
-# def dodaj(numer1,numer2):
-#     wynik = int(numer1) + int(numer2)
-#     return f'Wynik: {wynik}'
+@app.route('/mypage/contact', methods=['GET','POST'])
+def contact():
+    if request.method == 'GET':
+        return render_template('index2.html')
+    if request.method == 'POST':
+        message = request.form['message']
+        return render_template('message.html', message=message)
 
 if __name__ == '__main__':
     app.run(debug=True)
